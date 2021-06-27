dcfa5b0ea5b49a2e7ee96c7f689edc5c81ed467a
sameer Chauhan <48860013+sameer55chauhan@users.noreply.github.com>
2021-06-28 01:29:43 +0530

    UI Kitten setup half

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import 'react-native-gesture-handler';
 
 import React from 'react';
-import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, StyleSheet, StatusBar } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
 
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import HomeScreen from './screens/HomeScreen';
+import { default as theme } from './assets/app-theme/app-theme.json';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 export default App = () => {
   return (
-    <>
+    <NavigationContainer>
       <SafeAreaView>
-        <ApplicationProvider {...eva} theme={eva.light}>
+        <IconRegistry icons={EvaIconsPack} />
+        <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
           <HomeScreen />
         </ApplicationProvider>
       </SafeAreaView>
-    </>
+    </NavigationContainer>
   );
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,65 +1,70 @@
 import React from 'react';
-import { Layout, Text } from '@ui-kitten/components';
+import { Layout, Text, Icon, Button } from '@ui-kitten/components';
 import { PESDK, Configuration, TintMode } from 'react-native-photoeditorsdk';
 
 export default HomeScreen = () => {
-    
-  // const openEditor = () => {
-  //   // Set up sample image
-  //   let image = require('./assets/LA.jpg');
-  //   // Set up configuration
-  //   let configuration: Configuration = {
-  //     // Configure sticker tool
-  //     sticker: {
-  //       // Enable personal stickers
-  //       personalStickers: true,
-  //       // Configure stickers
-  //       categories: [
-  //         // Create sticker category with stickers
-  //         {
-  //           identifier: 'example_sticker_category_logos',
-  //           name: 'Logos',
-  //           thumbnailURI: require('./assets/React-Logo.png'),
-  //           items: [
-  //             {
-  //               identifier: 'example_sticker_logos_react',
-  //               name: 'React',
-  //               stickerURI: require('./assets/React-Logo.png'),
-  //             },
-  //             {
-  //               identifier: 'example_sticker_logos_imgly',
-  //               name: 'img.ly',
-  //               stickerURI: require('./assets/imgly-Logo.png'),
-  //               tintMode: TintMode.SOLID,
-  //             },
-  //           ],
-  //         },
-  //         // Use existing sticker category
-  //         { identifier: 'imgly_sticker_category_emoticons' },
-  //         // Modify existing sticker category
-  //         {
-  //           identifier: 'imgly_sticker_category_shapes',
-  //           items: [
-  //             { identifier: 'imgly_sticker_shapes_badge_01' },
-  //             { identifier: 'imgly_sticker_shapes_arrow_02' },
-  //             { identifier: 'imgly_sticker_shapes_spray_03' },
-  //           ],
-  //         },
-  //       ],
-  //     },
-  //   };
-  //   PESDK.openEditor(image, configuration).then(
-  //     (result) => {
-  //       console.log(result);
-  //     },
-  //     (error) => {
-  //       console.log(error);
-  //     },
-  //   );
-  // };
+
+    // const openEditor = () => {
+    //   // Set up sample image
+    //   let image = require('./assets/LA.jpg');
+    //   // Set up configuration
+    //   let configuration: Configuration = {
+    //     // Configure sticker tool
+    //     sticker: {
+    //       // Enable personal stickers
+    //       personalStickers: true,
+    //       // Configure stickers
+    //       categories: [
+    //         // Create sticker category with stickers
+    //         {
+    //           identifier: 'example_sticker_category_logos',
+    //           name: 'Logos',
+    //           thumbnailURI: require('./assets/React-Logo.png'),
+    //           items: [
+    //             {
+    //               identifier: 'example_sticker_logos_react',
+    //               name: 'React',
+    //               stickerURI: require('./assets/React-Logo.png'),
+    //             },
+    //             {
+    //               identifier: 'example_sticker_logos_imgly',
+    //               name: 'img.ly',
+    //               stickerURI: require('./assets/imgly-Logo.png'),
+    //               tintMode: TintMode.SOLID,
+    //             },
+    //           ],
+    //         },
+    //         // Use existing sticker category
+    //         { identifier: 'imgly_sticker_category_emoticons' },
+    //         // Modify existing sticker category
+    //         {
+    //           identifier: 'imgly_sticker_category_shapes',
+    //           items: [
+    //             { identifier: 'imgly_sticker_shapes_badge_01' },
+    //             { identifier: 'imgly_sticker_shapes_arrow_02' },
+    //             { identifier: 'imgly_sticker_shapes_spray_03' },
+    //           ],
+    //         },
+    //       ],
+    //     },
+    //   };
+    //   PESDK.openEditor(image, configuration).then(
+    //     (result) => {
+    //       console.log(result);
+    //     },
+    //     (error) => {
+    //       console.log(error);
+    //     },
+    //   );
+    // };
     return (
         <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text category='h1'>HOME</Text>
-        </Layout>
+            <Button accessoryLeft={FacebookIcon}>Login with Facebook</Button>
+        </Layout >
     )
 };
+
+const FacebookIcon = (props) => (
+    <Icon name='facebook' {...props} />
+);
